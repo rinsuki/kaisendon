59283f473a10b876b81a432119c96080bd066944
rinsuki <428rinsuki+git@gmail.com>
2018-03-26 02:11:41 +0900

    初期カラムを削除

--- src/index.ts
+++ src/index.ts
@@ -3,23 +3,6 @@ import app from "./app.vue"
 import "./global.css"
 import { store } from "./store";
 
-store.dispatch("addColumn", {
-    type: "timeline",
-    params: {
-        instance: "mstdn.maud.io",
-        type: "public:local",
-    }
-})
-
-store.dispatch("addColumn", {
-    type: "timeline",
-    params: {
-        instance: "mstdn.veryyyyyyyyyyyyyyyyyyyyyyyyyyylongdomain.example",
-        type: "hashtag",
-        hashtag: "veryyyyyyyyyyyyyyyyyyyyyyylonghashtag",
-    }
-})
-
 new Vue({
     el: "#app",
     render: h => h(app),
